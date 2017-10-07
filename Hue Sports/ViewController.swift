68932c23219d34643410f204b9798bca530dbef4
Morgan Bauer <morganbauer@Morgans-MacBook-Pro.local>
2017-10-06 21:22:53 -0700

    Initial Commit

---
+++ Hue Sports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hue Sports
+//
+//  Created by Morgan Bauer on 10/6/17.
+//  Copyright © 2017 Nue Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
